8d16c9e8a27cf5bba8a1f47b28c5f23ff4822e4e
Sergey Alyaev <cobxo3bot@gmail.com>
2025-10-05 13:20:24 +0200

    Updated the control program to ask for processing buttons

--- control.py
+++ control.py
@@ -1,9 +1,15 @@
 from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
 
 import sub
 
 hub = PrimeHub()
+hub.system.set_stop_button(Button.BLUETOOTH)
 
-print("Hello pybricks")
-sub.outside_fuction(hub)
-print("Hello pybricks")
+while True:
+    buttons_pressed = hub.buttons.pressed()
+    sub.perform_button_action(hub, buttons_pressed=buttons_pressed)
+
