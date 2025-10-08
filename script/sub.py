4d01b7850512d8d5fdf5b4e6871fc901fb8f9c10
Sergey Alyaev <cobxo3bot@gmail.com>
2025-10-08 09:43:11 +0200

    Endret print teksten

--- script/sub.py
+++ script/sub.py
@@ -1,13 +1,13 @@
-from pybricks.hubs import PrimeHub
+ from pybricks.hubs import PrimeHub
 from pybricks.parameters import Button
 
 def perform_button_action(hub: PrimeHub, buttons_pressed=None) -> None:
     if buttons_pressed is not None:
         if Button.LEFT in buttons_pressed:
-            print("Left")
+            print("Go Left")
             hub.display.char('<')
         elif Button.RIGHT in buttons_pressed:
-            print("Right")
+            print("Go Right")
             hub.display.char('>')
         else:
             hub.display.off()
